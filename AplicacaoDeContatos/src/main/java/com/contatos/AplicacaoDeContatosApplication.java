853d7ae13b7be97250349373d67f448f62608103
marciovdasilva <marciov.dasilva@gmail.com>
2022-08-27 14:43:35 -0300

    Commit do projeto 'AplicacaoDeContatos'.

---
+++ AplicacaoDeContatos/src/main/java/com/contatos/AplicacaoDeContatosApplication.java
@@ -0,0 +1,13 @@
+package com.contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoDeContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoDeContatosApplication.class, args);
+	}
+
+}
