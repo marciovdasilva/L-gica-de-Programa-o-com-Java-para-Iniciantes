e43dd36edc206714028e6c31d486f83e07f2eb38
marciovdasilva <marciov.dasilva@gmail.com>
2022-08-28 11:59:14 -0300

    Inicio do modulo '14-Criando sua primeira aplicação', criação da classe 'Criando a classe Contato'.

---
+++ AplicacaoDeContatos/src/main/java/com/contatos/Contato.java
@@ -0,0 +1,28 @@
+package com.contatos;
+
+public class Contato {
+	private String id;
+	private String nome;
+	private String telefone;
+	
+	String getId() {
+		return id;
+	}
+	void setId(String id) {
+		this.id = id;
+	}
+	String getNome() {
+		return nome;
+	}
+	void setNome(String nome) {
+		this.nome = nome;
+	}
+	String getTelefone() {
+		return telefone;
+	}
+	void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
